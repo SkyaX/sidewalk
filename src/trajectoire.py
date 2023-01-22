5e0d8979475a6708ce9bc1784f9e72281cb4f6d3
SkyaX <sky.axel.lans@gmail.com>
2023-01-22 17:39:35 +0100

    Add trajectoire.py with trajectoire's publisher

--- src/trajectoire.py
+++ src/trajectoire.py
@@ -31,6 +31,37 @@ class OBJ :
 		self.Xc = int(((self.xmax-self.xmin)/2)+self.xmin)
 		self.Yc = int(((self.ymax-self.ymin)/2)+self.ymin)
 
+def stackImages(scale,imgArray):
+	rows = len(imgArray)
+	cols = len(imgArray[0])
+	rowsAvailable = isinstance(imgArray[0], list)
+	width = imgArray[0][0].shape[1]
+	height = imgArray[0][0].shape[0]
+	if rowsAvailable:
+		for x in range ( 0, rows):
+			for y in range(0, cols):
+				if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+					imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+				else:
+					imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+				if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+		imageBlank = np.zeros((height, width, 3), np.uint8)
+		hor = [imageBlank]*rows
+		hor_con = [imageBlank]*rows
+		for x in range(0, rows):
+			hor[x] = np.hstack(imgArray[x])
+		ver = np.vstack(hor)
+	else:
+		for x in range(0, rows):
+			if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+				imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+			else:
+				imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+			if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+		hor= np.hstack(imgArray)
+		ver = hor
+	return ver
+
 def get_subs():
 	rospy.Subscriber('/darknet_ros/bounding_boxes', BBs,  process_BBs)
 
@@ -46,6 +77,13 @@ def process_BBs(BBS):
 	global OBJs
 
 	OBJs = [ OBJ(bb) for bb in BBS.bounding_boxes if bb.probability>0.30 ]
+	
+	Pose_obj = Twist()
+	Pose_obj.linear.x = OBJs[0].Xc
+	Pose_obj.linear.y = OBJs[0].Yc
+	Pose_obj.linear.z = cv_dep_image[OBJs[0].Xc,OBJs[0].Yc]
+
+	Traj_publisher.publish(Pose_obj)
 
 def process_RGBD(cam_image, depth_image):
 	
@@ -62,6 +100,18 @@ def process_RGBD(cam_image, depth_image):
 	cv_cam_image = bridge.imgmsg_to_cv2(CAM_IMG, desired_encoding='passthrough')
 	cv_dep_image = bridge.imgmsg_to_cv2(DEP_IMG, desired_encoding='passthrough')
 	# plt.imshow(cv_dep_image); plt.show()
+
+	try :
+		yolo_means = cv_cam_image.copy()
+		IMGs = []
+		for object in OBJs :
+			object_img = yolo_means[object.ymin:object.ymax,object.xmin:object.xmax]
+			IMGs.append(object_img)
+		
+		objects_imgs = stackImages(0.75,IMGs)
+		cv2.imshow("Object", objects_imgs); cv2.waitKey(1)
+
+	except : print("Waiting for the darknet topic")
 
 	cv_dep_rgb = cv2.normalize(cv_dep_image, None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype=cv2.CV_8U)
 	cv_dep_rgb = cv2.cvtColor(cv_dep_rgb, cv2.COLOR_GRAY2RGB, 0);
@@ -93,11 +143,12 @@ def process_dir(direction):
 	print(direction)
 
 if __name__ == '__main__':
-	rospy.init_node('yolo_centroids')
+	rospy.init_node('yolo_centroids', anonymous = True)
 
 	global bridge
 	bridge = CvBridge()
 
+	Traj_publisher = rospy.Publisher('trajectoire', Twist, queue_size = 1)
 	get_subs()
 	
 	rospy.spin()
